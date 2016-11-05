2ce5fc4493bf1d5bcaa668265867038e7a132f76
Gabriel25 <barnoth.g@gmail.com>
2016-11-05 17:19:24 +0100

    Import added

--- core/src/com/race2135/game/PlayScreen.java
+++ core/src/com/race2135/game/PlayScreen.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
