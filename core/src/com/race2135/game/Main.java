36ef0780d6326f1f06b5c14859078bbf4998166e
Gabriel25 <barnoth.g@gmail.com>
2016-12-07 22:15:57 +0100

    Removed global screen instances, probably not necessary in a rush.

--- core/src/com/race2135/game/Main.java
+++ core/src/com/race2135/game/Main.java
@@ -3,23 +3,21 @@ package com.race2135.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.Box2D;
 
 public class Main extends Game {
-    public Screen menuScreen, playScreen, carSelectionScreen;
+    public static Texture black;
 
 	@Override
-	public void create () {
+	public void create() {
         Box2D.init();
 
         CarInfo.init();
         LevelInfo.init();
+        black = new Texture("black.png");
 
-        menuScreen = new MainMenu(this);
-        playScreen = new PlayScreen(this, CarInfo.models.get(0), LevelInfo.levels.get(0));
-        carSelectionScreen = new CarSelectionMenu(this);
-
-        setScreen(menuScreen);
+        setScreen(new MainMenu(this));
 	}
 }
